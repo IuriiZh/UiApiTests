00743fb8b758140e40613e709cf7361e26319792
IuriiZh <zatwhys@gmail.com>
2025-11-03 12:15:55 -0500

    Change Property reader

--- src/main/java/utils/PropertyReader.java
+++ src/main/java/utils/PropertyReader.java
@@ -9,11 +9,20 @@ import java.util.Properties;
 // Singleton pattern
 public class PropertyReader {
     @Getter
-    private final static PropertyReader instance = new PropertyReader();
+    private static PropertyReader instance = new PropertyReader();
     private final Properties properties;
 
     public static String UiUrl = PropertyReader.getInstance().getUiUrl();
 
+    public static String ApiUrl = PropertyReader.getInstance().getApiUrl();
+    public static String ApiKeyHeader = PropertyReader.getInstance().getApiKeyHeader();
+    public static String ApiKey = PropertyReader.getInstance().getApiKey();
+    public static String ApiUsername = PropertyReader.getInstance().getApiUsername();
+    public static String ApiEmail = PropertyReader.getInstance().getApiEmail();
+    public static String ApiPassword = PropertyReader.getInstance().getApiPassword();
+
+    public static String ApiId = PropertyReader.getInstance().getApiId();
+    public static String ApiJob = PropertyReader.getInstance().getApiJob();
 
     @SneakyThrows
     public PropertyReader() {
@@ -22,16 +31,29 @@ public class PropertyReader {
             properties.load(reader);
         }
     }
+    public String getHeadless() { return properties.getProperty("headless"); }
+    public String getBrowser() {return properties.getProperty("browser");}
 
-    public String getHeadless() {
-        return properties.getProperty("headless");
+    public String getUiUrl(){ return properties.getProperty("UI_url");}
+    public String getUIPassword(){
+        return properties.getProperty("UI_password");
     }
-    public String getBrowser() {
-        return properties.getProperty("browser");
-    }
-
-    public String getUiUrl() {
-        return properties.getProperty("UI_url");
+    public String getUIUsername(){
+        return properties.getProperty("UI_username");
     }
 
+    public String getApiUrl(){
+        return properties.getProperty("API_url");
+    }
+    public String getApiKeyHeader(){
+        return properties.getProperty("API_KeyHeader");
+    }
+    public String getApiKey(){ return properties.getProperty("API_Key"); }
+
+    public String getApiUsername() {return properties.getProperty("ApiUsername");}
+    public String getApiEmail() {return properties.getProperty("ApiEmail");}
+    public String getApiPassword() {return properties.getProperty("ApiPassword");}
+
+    public String getApiId() {return properties.getProperty("ApiId");}
+    public String getApiJob() {return properties.getProperty("ApiJob");}
 }
